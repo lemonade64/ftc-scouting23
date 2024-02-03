b0ccb891e64b56830e31a1d63f0ef5d46e7b61ad
Kevin Hou <strawberrypi64@gmail.com>
2024-02-03 14:05:00 +1100

    thank you page

---
+++ src/app/thank-you/page.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function ThankYou() {
+  return (
+    <main className="grid h-screen place-items-center px-6 py-12 sm:py-32">
+      <div className="text-center">
+        <h1 className="mt-4 font-bold text-black text-5xl">Submitted!</h1>
+        <p className="mt-6 text-base text-gray-600">
+          Thanks for submitting the form.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            href={"/"}
+            className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm"
+          >
+            Submit Another Form
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+}
