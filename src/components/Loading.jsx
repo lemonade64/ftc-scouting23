9cab42acf087855f120dc1904e84a4a8b66e67a2
Kevin Hou <strawberrypi64@gmail.com>
2024-02-20 17:21:22 +1100

    api fetch skeleton

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,125 +2,65 @@
 
 import { Disclosure } from "@headlessui/react";
 import { ChevronUpIcon } from "@heroicons/react/20/solid";
+import disclosure from "@/data/disclosure";
 
 export default function Loading() {
   return (
     <div className="p-10">
       <div className="animate-pulse px-4 sm:px-0">
-        <h1 className="rounded-full bg-slate-700 h-4 w-20 my-4"></h1>
-        <p className="rounded-full bg-slate-700 h-4 w-40"></p>
+        <h1 className="rounded-full bg-slate-700 h-4 text-transparent w-fit my-4">
+          Team Data
+        </h1>
+        <p className="rounded-full bg-slate-700 h-3 text-transparent w-fit">
+          Performance Statistics
+        </p>
       </div>
       <div className="mt-6 border-t border-gray-400">
         <dl className="divide-y divide-gray-400">
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 animate-pulse">
             <dt className="leading-6 rounded-full bg-slate-700 h-2 text-transparent w-fit">
-              Team Number
+              Team Number:
             </dt>
             <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0 bg-slate-700 h-2 text-transparent rounded-full w-fit">
               11148
             </dd>
           </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 animate-pulse">
-            <dt className="leading-6 rounded-full bg-slate-700 h-2 text-transparent w-fit">
-              Team Name
-            </dt>
-            <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0 bg-slate-700 h-2 text-transparent rounded-full w-fit">
-              Greybacks
-            </dd>
-          </div>
-
-          <Disclosure>
-            {({ open }) => (
-              <>
-                <Disclosure.Button className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 animate-pulse">
-                  <dt className="leading-6 rounded-full bg-slate-700 h-2 text-transparent w-fit">
-                    Autonomous OPR
-                  </dt>
-                  <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0 bg-slate-700 h-2 text-transparent rounded-full w-fit">
-                    000
-                  </dd>
-                  <ChevronUpIcon
-                    className={`${
-                      open ? "rotate-180 " : ""
-                    } h-5 w-5 transition-all duration-300 transform`}
-                  />
-                </Disclosure.Button>
-                <Disclosure.Panel className="!animate-none divide-y divide-black">
-                  <div className="mx-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 animate-pulse">
+          {disclosure.map(({ id, title, metrics }) => (
+            <Disclosure key={id}>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button className="px-4 py-6 sm:flex sm:flex-cols sm:gap-x-4 sm:px-0 w-full animate-pulse">
+                    <ChevronUpIcon
+                      className={`${
+                        open ? "rotate-180 " : ""
+                      } h-4 w-4 transition-all duration-300 transform`}
+                    />
                     <dt className="leading-6 rounded-full bg-slate-700 h-2 text-transparent w-fit">
-                      Team Prop
+                      {title} OPR:
                     </dt>
                     <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0 bg-slate-700 h-2 text-transparent rounded-full w-fit">
-                      Yes
+                      111
                     </dd>
-                  </div>
-                  <div className="mx-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 animate-pulse">
-                    <dt className="leading-6 rounded-full bg-slate-700 h-2 text-transparent w-fit">
-                      Yellow Pixel
-                    </dt>
-                    <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0 bg-slate-700 h-2 text-transparent rounded-full w-fit">
-                      Yes
-                    </dd>
-                  </div>
-                  <div className="mx-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 animate-pulse">
-                    <dt className="leading-6 rounded-full bg-slate-700 h-2 text-transparent w-fit">
-                      Purple Pixel
-                    </dt>
-                    <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0 bg-slate-700 h-2 text-transparent rounded-full w-fit">
-                      Yes
-                    </dd>
-                  </div>
-                </Disclosure.Panel>
-              </>
-            )}
-          </Disclosure>
-
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 animate-pulse">
-            <dt className="leading-6 rounded-full bg-slate-700 h-2 text-transparent w-fit">
-              TeleOp OPR
-            </dt>
-            <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0 bg-slate-700 h-2 text-transparent rounded-full w-fit">
-              000
-            </dd>
-          </div>
-
-          <Disclosure>
-            {({ open }) => (
-              <>
-                <Disclosure.Button className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 animate-pulse">
-                  <dt className="leading-6 rounded-full bg-slate-700 h-2 text-transparent w-fit">
-                    Endgame OPR
-                  </dt>
-                  <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0 bg-slate-700 h-2 text-transparent rounded-full w-fit">
-                    000
-                  </dd>
-                  <ChevronUpIcon
-                    className={`${
-                      open ? "rotate-180 " : ""
-                    } h-5 w-5 transition-all duration-300 transform`}
-                  />
-                </Disclosure.Button>
-                <Disclosure.Panel className="!animate-none divide-y divide-black">
-                  <div className="mx-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 animate-pulse">
-                    <dt className="leading-6 rounded-full bg-slate-700 h-2 text-transparent w-fit">
-                      Drone
-                    </dt>
-                    <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0 bg-slate-700 h-2 text-transparent rounded-full w-fit">
-                      Yes
-                    </dd>
-                  </div>
-                  <div className="mx-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 animate-pulse">
-                    <dt className="leading-6 rounded-full bg-slate-700 h-2 text-transparent w-fit">
-                      Climb
-                    </dt>
-                    <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0 bg-slate-700 h-2 text-transparent rounded-full w-fit">
-                      Yes
-                    </dd>
-                  </div>
-                </Disclosure.Panel>
-              </>
-            )}
-          </Disclosure>
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="!animate-none divide-y divide-gray-400">
+                    {metrics.map(({ id, name }) => (
+                      <div
+                        key={id}
+                        className="ml-4 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 animate-pulse"
+                      >
+                        <dt className="leading-6 rounded-full bg-slate-700 h-2 text-transparent w-fit">
+                          {name}:
+                        </dt>
+                        <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0 bg-slate-700 h-2 text-transparent rounded-full w-fit">
+                          aaa
+                        </dd>
+                      </div>
+                    ))}
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          ))}
         </dl>
       </div>
     </div>
---
+++ src/data/disclosure.js
@@ -0,0 +1,39 @@
+export const disclosure = [
+  {
+    id: 1,
+    title: "Autonomous",
+    metrics: [
+      {
+        id: 1,
+        name: "Team Prop",
+      },
+      { id: 2, name: "Yellow Pixel" },
+      { id: 3, name: "Purple Pixel" },
+    ],
+  },
+  {
+    id: 2,
+    title: "TeleOp",
+    metrics: [
+      {
+        id: 1,
+        name: "Mosaics",
+      },
+      { id: 2, name: "Set Line" },
+      { id: 3, name: "Penalties" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Endgame",
+    metrics: [
+      {
+        id: 1,
+        name: "Drone",
+      },
+      { id: 2, name: "Climb" },
+    ],
+  },
+];
+
+export default disclosure;
