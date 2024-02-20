56c0d8b72fbc11cf21d79cac7d5d51c1eb0bb34a
Kevin Hou <strawberrypi64@gmail.com>
2024-02-20 18:20:57 +1100

    button submission state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -363,8 +363,29 @@ export default function Form() {
             <button
               type="submit"
               disabled={buttonState}
-              className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm"
+              className="inline-flex rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm"
             >
+              <svg
+                class={`animate-spin -ml-1 mr-3 h-5 w-5 text-white ${
+                  buttonState ? "" : "hidden"
+                }`}
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
               {buttonState ? "Submitting..." : "Submit"}
             </button>
           </div>
